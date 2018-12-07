62c77bd2941fd198346954709357e3ce358cdc9c
Surabhi Lodha <surabhil@usc.edu>
2018-12-06 17:09:53 -0800

    update ore.usagelog.cpp to be compatible with eos v1.5

--- contracts/ore.usage_log/ore.usage_log.cpp
+++ contracts/ore.usage_log/ore.usage_log.cpp
@@ -1,21 +1,20 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/time.hpp>
 #include <chrono>
 
 using namespace eosio;
 using namespace std;
 
-class usage_log : public eosio::contract
+class [[eosio::contract("ore.usage_log")]] usage_log : public eosio::contract
 {
   public:
-    usage_log(account_name self) : eosio::contract(self) {}
+    using contract::contract;
+    usage_log(name receiver, name code, datastream<const char*> ds): contract(receiver, code, ds), _logs(receiver, receiver.value){}
 
-    //@abi action
+    [[eosio::action]]
     void createlog(uint64_t instrument_id, string right_name, string token_hash, uint64_t timestamp)
     {
-
-        logs_table _logs(_self, _self);
-
         uint64_t right_hash = hashStr(right_name);
 
         _logs.emplace(_self, [&](auto &a) {
@@ -29,11 +28,9 @@ class usage_log : public eosio::contract
 
     // TODO: require authority of the reconciler
     // delets the entry from the log table with matching token_hash
-    // @abi action
+    [[eosio::action]]
     void deletelog(uint64_t instrument_id, string token_hash)
     {
-        logs_table _logs(_self, _self);
-
         auto itr = _logs.find(hashStr(token_hash));
 
         eosio_assert(itr != _logs.end(), "No log exist for the given pair or right and instrument");
@@ -43,12 +40,10 @@ class usage_log : public eosio::contract
         _logs.erase(itr);
     }
 
-    //@abi action
+    [[eosio::action]]
     void updatecount(uint64_t instrument_id, string right_name, asset cpu)
     {
         uint64_t right_hash = hashStr(right_name);
-
-        //Test the scoping
 
         counts_table _counts(_self, instrument_id);
 
@@ -59,7 +54,7 @@ class usage_log : public eosio::contract
             _counts.emplace(_self, [&](auto &a) {
                 a.right_hash = right_hash;
                 a.right_name = right_name;
-                a.last_usage_time = now();
+                a.last_usage_time = time_point_sec(now());
                 a.total_count = 1;
                 a.total_cpu = cpu;
             });
@@ -67,7 +62,7 @@ class usage_log : public eosio::contract
         else
         {
             _counts.modify(itr, _self, [&](auto &a) {
-                a.last_usage_time = now();
+                a.last_usage_time = time_point_sec(now());
                 a.total_count += 1;
                 a.total_cpu += cpu;
             });
@@ -75,8 +70,7 @@ class usage_log : public eosio::contract
     }
 
   private:
-    //@abi table logs i64
-    struct log
+    struct [[eosio::table]] log
     {
         uint64_t instrument_id;
         uint64_t right_hash;
@@ -89,25 +83,26 @@ class usage_log : public eosio::contract
     };
 
     //following structure enables fast query of api call count for the verifier
-    //@abi table counts i64
-    struct callcount
+    struct [[eosio::table]] callcount
     {
         uint64_t right_hash;
         string right_name;
-        time last_usage_time;
+        time_point_sec last_usage_time;
         uint64_t total_count;
         asset total_cpu;
         auto primary_key() const { return right_hash; }
 
         EOSLIB_SERIALIZE(callcount, (right_hash)(right_name)(last_usage_time)(total_count)(total_cpu))
     };
+    
+    typedef eosio::multi_index<"counts"_n, callcount> counts_table;
+    typedef eosio::multi_index<"logs"_n, log> logs_table;
 
-    typedef eosio::multi_index<N(counts), callcount> counts_table;
-    typedef eosio::multi_index<N(logs), log> logs_table;
+    logs_table _logs;
 
     uint64_t hashStr(const string &strkey)
     {
         return hash<string>{}(strkey);
     }
 };
-EOSIO_ABI(usage_log, (createlog)(deletelog)(updatecount))
+EOSIO_DISPATCH(usage_log, (createlog)(deletelog)(updatecount))

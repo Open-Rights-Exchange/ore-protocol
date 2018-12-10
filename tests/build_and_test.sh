b4b4a30a10b4e99dcb8434d55c145cc21ca69e42
Başarcan Celebci <basar@Basarcans-MacBook-Pro.local>
2018-12-10 19:52:04 +0300

    ci added

---
+++ tests/build_and_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ~/Workspace/testeosfactory/contracts
+
+cd ore.instrument && mkdir build
+
+eosiocpp -g build/ore.instrument.wast ore.instrument.cpp
+eosiocpp -o build/ore.instrument.abi ore.instrument.cpp
+
+cd ../ore.rights_registry && mkdir build
+eosiocpp -g build/ore.rights_registry.wast ore.rights_registry.cpp
+eosiocpp -o build/ore.rights_registry.abi ore.rights_registry.cpp
+
+cd ../ore.usage_log && mkdir build
+eosiocpp -g build/ore.usage_log.wast ore.usage_log.cpp
+eosiocpp -o build/ore.usage_log.abi ore.usage_log.cpp
+
+cd ../ore.standard_token && mkdir build
+eosiocpp -g build/ore.standard_token.wast ore.standard_token.cpp
+eosiocpp -o build/ore.standard_token.abi ore.standard_token.cpp
+
+cd ../tests
+
+python3 standard_token.py
+python3 rights_registry.py
+python3 instrument.py
+python3 usage_log.py

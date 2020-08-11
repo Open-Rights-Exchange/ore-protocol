e96bedfced22e940be7fb61dcf00aed7db0137d1
basarrcan <basarrcan@gmail.com>
2020-08-11 19:05:43 +0300

    deltoken deleted

--- contracts/ore.apptoken/include/ore.apptoken.hpp
+++ contracts/ore.apptoken/include/ore.apptoken.hpp
@@ -32,8 +32,6 @@ public:
           */
    ACTION create(const name &issuer,
                  const asset &maximum_supply);
-   ACTION deltoken( const name&   owner,
-                    const symbol&  symbol );
    ACTION issue(const name &to, const asset &quantity, const string &memo);
    ACTION retire(const asset &quantity, const string &memo);
    ACTION transfer(const name &from,

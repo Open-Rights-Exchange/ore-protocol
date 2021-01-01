097028f693615fdcf1c29ea15ebfb0ee3c24b4e5
basarrcan <basarrcan@gmail.com>
2021-01-01 18:08:56 +0000

    remove comments

--- scripts/2020-12_TokenEconomicsUpgrade/test-environment/test.sh
+++ scripts/2020-12_TokenEconomicsUpgrade/test-environment/test.sh
@@ -24,16 +24,3 @@ else
   ../upgrade/step2-verify.sh "$cleos" /root
   ./chain-setup/stop.sh "$cleos" /root
 fi
-
-
-# cleos="$1"
-# setup_dir="$2"
-
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# if [[ "$setup_dir" == "" ]]; then
-#   setup_dir="/root"
-# fi
-
-# $DIR/setup-v1.sh "$cleos" "$setup_dir"
-# $DIR/upgrade-v2.sh "$cleos" "$setup_dir"

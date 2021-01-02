f8fe4a8e6c1220282c943e45bfb5d4f22788974d
basarrcan <basarrcan@gmail.com>
2021-01-02 01:59:42 +0000

    update refactoring

--- scripts/2020-12_TokenEconomicsUpgrade/upgrade/step1-upgrade.sh
+++ scripts/2020-12_TokenEconomicsUpgrade/upgrade/step1-upgrade.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
+#
+# To run the upgrade there must be a wallet unlock with following keys:
+# eosio@active, eosio.token@active, createescrow@active, system.ore@owner, system.ore@active
+#
 cleos="$1"
 eosio_contracts=${2:-~}/contracts-v2/eosio.contracts
 createescrow_path=${2:-~}/contracts-v2/CreateEscrow
 if [[ "$2" == "/root" ]]; then
   ore_contracts="$2/contracts"
 else
-  ore_contracts="$2/../../../../contracts"
+  ore_contracts="../../../contracts"
 fi
 
-sleep 2s
-$cleos system newaccount -x 1000 eosio --transfer eosio.upay EOS6H2tjbrS6zm8d3tX6yeHorEoihP23Ny9c9wFRHGfJp4vtD42rn --stake-net "10000.0000 SYS" --stake-cpu "10000.0000 SYS" --buy-ram-kbytes 8192 -p eosio
-sleep 2s
-$cleos set contract -x 1000 eosio $eosio_contracts/eosio.system/
-sleep 2s
-$cleos set contract -x 1000 eosio.token $eosio_contracts/eosio.token/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sleep 2s
-$cleos set contract -x 1000 system.ore $ore_contracts/build/ore.system/ ore.system.wasm ore.system.abi
-
+$DIR/upgradeScripts/step1-unstake-ram.sh $cleos 
 sleep 2s
-$cleos set contract -x 1000 createescrow $createescrow_path/ createescrow.wasm createescrow.abi
+$DIR/upgradeScripts/step2-update-eosio.sh "$cleos" "$eosio_contracts"
 sleep 2s
-$cleos push action -x 1000 eosio setram '[17179869184]' -p eosio # 16 Gib
+$DIR/upgradeScripts/step3-update-ore-system.sh "$cleos" "$ore_contracts"
 sleep 2s
-$cleos set account permission -x 1000 system.ore active '{"threshold": 1,"keys": [{"key": "EOS6H2tjbrS6zm8d3tX6yeHorEoihP23Ny9c9wFRHGfJp4vtD42rn","weight": 1}],"accounts": [{"permission":{"actor":"eosio","permission":"eosio.code"},"weight":1}, {"permission":{"actor":"system.ore","permission":"eosio.code"},"weight":1}]}' owner -p system.ore@owner
-sleep 2s
-$cleos push action -x 1000 eosio.token open '["eosio.upay", "4,ORE", "eosio"]' -p eosio@active
-sleep 2s
-$cleos push action -x 1000 eosio.token setstaked '["0.0000 ORE"]' -p eosio.token@active
-sleep 2s
-$cleos push action -x 1000 eosio setinflation '[0, 10000, 10000]' -p eosio@active
-sleep 2s
-$cleos push action -x 1000 system.ore migrate '[]' -p system.ore
+$DIR/upgradeScripts/step4-update-createescrow.sh "$cleos" "$createescrow_path"

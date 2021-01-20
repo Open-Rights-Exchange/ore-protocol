f4c18608cb7f370f14a37b02bede509acee938d4
basarrcan <basarrcan@gmail.com>
2021-01-20 21:14:01 +0000

    update deployment scripts auths for setabi change

--- scripts/2020-12_TokenEconomicsUpgrade/upgrade/upgradeScripts/step2-update-eosio.sh
+++ scripts/2020-12_TokenEconomicsUpgrade/upgrade/upgradeScripts/step2-update-eosio.sh
@@ -4,9 +4,9 @@ eosio_contracts="$2"
 sleep 2s
 $cleos system newaccount -x 1000 eosio --transfer eosio.upay EOS6H2tjbrS6zm8d3tX6yeHorEoihP23Ny9c9wFRHGfJp4vtD42rn --stake-net "10000.0000 SYS" --stake-cpu "10000.0000 SYS" --buy-ram-kbytes 8192 -p eosio
 sleep 2s
-$cleos set contract -x 1000 eosio $eosio_contracts/eosio.system/
+$cleos set contract -x 1000 eosio $eosio_contracts/eosio.system/ -p eosio -p system.ore
 sleep 2s
-$cleos set contract -x 1000 eosio.token $eosio_contracts/eosio.token/
+$cleos set contract -x 1000 eosio.token $eosio_contracts/eosio.token/ -p eosio.token -p system.ore
 sleep 2s
 $cleos push action -x 1000 eosio setram '[17179869184]' -p eosio # 16 Gib
 sleep 2s
